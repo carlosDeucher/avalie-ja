c3538ba273001bdb495ab27b331935e60ebafc3a
carlosDeucher <carloshenriquedeucher@gmail.com>
2023-04-18 21:11:25 -0300

    hook q retorna o breakpoint

---
+++ src/hooks/useBreakpoint.jsx
@@ -0,0 +1,27 @@
+import { getDeviceSize } from "@/utils/handleResponsiveValues";
+import { useEffect, useState } from "react";
+
+export default function useBreakpoint() {
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [breakpoint, setBreakpoint] = useState(null);
+
+  useEffect(() => {
+    // only execute all the code below in client side
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setBreakpoint(getDeviceSize(window.innerWidth));
+    }
+
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+  return breakpoint;
+}
