b923d7882b1efb2b69300aaf1f1f1d00c7356959
carlosDeucher <carloshenriquedeucher@gmail.com>
2023-05-09 22:27:03 -0300

    [add] api buscar email

---
+++ src/pages/api/user/mutation/get_user.js
@@ -0,0 +1,15 @@
+import handleApiUnAuth from "@/lib/handleApiUnAuth";
+=import Users from "models/user";
+
+export default handleApiUnAuth().post(async (req, res) => {
+  try {
+    const user = await Users.findOne({ where: { email: req.body.email } });
+    if (user) {
+      res.json({ type: "USER_FOUND", status: "success" });
+    } else {
+      res.status(200).json({ status: "error", type: "USER_NOT_FOUND" });
+    }
+  } catch (e) {
+    res.status(500).json({ status: "error", type: "UNKNOWN_ERROR" });
+  }
+});
