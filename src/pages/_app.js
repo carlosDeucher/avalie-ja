a99b0f952cb282a6c42a3466b369aedd2289e97b
carlosDeucher <carloshenriquedeucher@gmail.com>
2023-05-11 20:21:18 -0300

    [add] fonte , cor de fundo e reset css

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,4 @@
 import ThemeProvider from "@/contexts/ThemeProvider";
-import "../views/styles/_reset.scss";
 import GlobalStyle from "@/views/styles/globalStyle";
 export default function App({ Component, pageProps }) {
   return (
