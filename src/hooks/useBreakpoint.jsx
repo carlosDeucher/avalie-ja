8764a19147660c31f2711b53f141c2fb1341cea6
carlosDeucher <carloshenriquedeucher@gmail.com>
2023-05-10 22:13:57 -0300

    [update] getDeviceSize arquivo separado

--- src/hooks/useBreakpoint.jsx
+++ src/hooks/useBreakpoint.jsx
@@ -1,5 +1,5 @@
 import { ContextTheme } from "@/contexts/ThemeProvider";
-import { getDeviceSize } from "@/utils/handleResponsiveValues";
+import  getDeviceSize  from "@/utils/getDeviceSize.js";
 import { useContext, useEffect, useState } from "react";
 
 export default function useBreakpoint() {
