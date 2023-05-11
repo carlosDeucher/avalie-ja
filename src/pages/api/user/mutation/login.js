620b600e03e9756674c0ab54443fae4409404125
carlosDeucher <carloshenriquedeucher@gmail.com>
2023-05-10 22:20:44 -0300

    [remove] consoles e comentarios

--- src/pages/api/user/mutation/login.js
+++ src/pages/api/user/mutation/login.js
@@ -1,47 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import database from "config/postgresdb";
-import User from "models/user";
-import Produto from "models/produto";
-import Comment from "models/comment";
+import Users from "models/user";
+import Products from "models/produto";
+import Comments from "models/comment";
 import handleApiUnAuth from "@/lib/handleApiUnAuth";
-
-// export default async function (req, res){
-//   try {
-//     console.log(req.body);
-//     const userLogged = await User.findOne({
-//       where: {
-//         email: req.body.email,
-//         password: req.body.password,
-//       },
-//     });
-//     console.log(userLogged);
-
-//     if (userLogged) {
-//       req.session.user = {
-//         email: req.body.email,
-//         password: req.body.password,
-//       };
-//       await req.session.save();
-//       res.json({ type: "USER_LOGGED", status: "success" });
-//     } else {
-//       res.status(400).json({ status: "error", type: "INVALID_CREDENCIALS" });
-//     }
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json({ status: "error", type: "UNKNOWN_ERROR" });
-//   }
-// };
 
 export default handleApiUnAuth().post(async (req, res) => {
   try {
-    console.log(req.body);
-    const userLogged = await User.findOne({
+    const userLogged = await Users.findOne({
       where: {
         email: req.body.email,
         password: req.body.password,
       },
     });
-    console.log(userLogged);
 
     if (userLogged) {
       req.session.user = {
@@ -54,7 +25,6 @@ export default handleApiUnAuth().post(async (req, res) => {
       res.status(400).json({ status: "error", type: "INVALID_CREDENCIALS" });
     }
   } catch (err) {
-    console.log(err);
     res.status(500).json({ status: "error", type: "UNKNOWN_ERROR" });
   }
 });
