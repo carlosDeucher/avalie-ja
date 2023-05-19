6a035f33fe641602fba918769ce9829d22e80281
carlosDeucher <carloshenriquedeucher@gmail.com>
2023-05-18 22:07:10 -0300

    [update] sombra de erro, InputError, register Uma sombra de erro é exibida sem o focus, passando retorno de register

--- src/views/components/ui/input/Input.jsx
+++ src/views/components/ui/input/Input.jsx
@@ -3,67 +3,70 @@
 import useInlineStyle from "@/hooks/useInlineStyles";
 import { useContext, useRef } from "react";
 import { ThemeContext } from "@/contexts/ThemeProvider";
+import InputError from "../error/InputError";
+import Box from "../../estructure/box/Box";
 export default function Input(props) {
   const {
-    register = () => null, //register e name sao utilizados para controlar o input com o useForm
-    name,
+    register, //register  utilizados para controlar o input com o useForm
     sp,
     error,
     labelText,
   } = props;
   const commonProps = { ...props };
   delete commonProps.register;
-  delete commonProps.name;
   delete commonProps.sp;
   delete commonProps.error;
   delete commonProps.labelText;
 
   const { fontSizes, colors } = useContext(ThemeContext);
   const style = useInlineStyle(sp);
-  const inputRef = useRef(null);
+  const registerHookForm = register;
+
   return (
-    <div css={{ ...style }}>
-      <label
-        css={{
-          fontSize: fontSizes["small"],
-          marginBottom: "4px",
-          display: "inline-block",
-        }}
-      >
-        {labelText}
-      </label>
-      <div
-        onClick={() => inputRef.current.focus()}
-        css={{
-          border: "1px solid " + colors.grey[3],
-          boxShadow: "0px 8px 24px" + colors.grey[3],
-          display: "flex",
-          alignItems: "center",
-          padding: "10px 5px",
-          borderRadius: "0.375em",
-          "&:focus-within": {
-            boxShadow: error
-              ? `0 0 0 1px ${colors.error.main}`
-              : `0 0 0 0.125em ${colors.secondary.main}`,
-          },
-          outline: "none",
-          flex: 1,
-        }}
-      >
-        <input
-          {...register(name)}
-          ref={inputRef}
-          {...commonProps}
+    <Box sp={{ ...style }}>
+      <label css={{ display: "block" }}>
+        <span
           css={{
-            border: "none",
-            outline: "none",
-            fontSize: fontSizes["medium"],
-            lineHeight: "1.5rem",
-            flexGrow: 1,
-            backgroundColor: colors.white,
+            fontSize: fontSizes["small"],
+            marginBottom: "4px",
+            display: "inline-block",
           }}
-        ></input>
-      </div>
-    </div>
+        >
+          {labelText}
+        </span>
+        <div
+          css={{
+            border: "1px solid " + colors.grey[3],
+            boxShadow: error && `0 0 0 1px ${colors.error.main}`,
+            display: "flex",
+            alignItems: "center",
+            padding: "10px 5px",
+            borderRadius: "0.375em",
+            "&:focus-within": {
+              boxShadow: error
+                ? `0 0 0 1.5px ${colors.error.main}`
+                : `0 0 0 0.125em ${colors.secondary.main}`,
+            },
+            outline: "none",
+            flex: 1,
+          }}
+        >
+          <input
+            {...commonProps}
+            {...registerHookForm}
+            css={{
+              border: "none",
+              outline: "none",
+              fontSize: fontSizes["medium"],
+              lineHeight: "1.5rem",
+              flexGrow: 1,
+              backgroundColor: colors.white,
+            }}
+            id="input"
+          ></input>
+        </div>
+      </label>
+      <InputError showError={!!error}>{error}</InputError>
+    </Box>
   );
 }
