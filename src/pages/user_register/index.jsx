c4d5ba1997649f66ea3ad81392b7086b56b0bf57
carlosDeucher <carloshenriquedeucher@gmail.com>
2023-05-24 22:11:41 -0300

    [add] snackbar message

--- src/pages/user_register/index.jsx
+++ src/pages/user_register/index.jsx
@@ -1,3 +1,4 @@
+import Snackbar from "@/views/components/estructure/alert/Snackbar";
 import Loading from "@/views/components/ui/loading/Loading";
 import UserRegisterView from "@/views/pageViews/userRegister";
 import axios from "axios";
@@ -6,6 +7,7 @@ import React, { createContext, useState } from "react";
 export const ApiUserRegisterContext = createContext();
 export default function UserRegister() {
   const [isLoading, setIsLoading] = useState(false);
+  const [showError, setShowError] = useState(false);
   const createUser = async (form) => {
     setIsLoading(true);
     try {
@@ -14,10 +16,12 @@ export default function UserRegister() {
         username: form.username,
         password: form.password,
       });
+      
       setIsLoading(false);
       return data;
     } catch (e) {
       console.log(e);
+      setShowError(true);
       setIsLoading(false);
       return null;
     }
@@ -26,6 +30,9 @@ export default function UserRegister() {
   return (
     <>
       <Loading isOpen={isLoading} />
+      <Snackbar isOpen={showError} onClose={() => setShowError(false)}>
+        Falha ao cadastrar usuario
+      </Snackbar>
       <ApiUserRegisterContext.Provider value={{ createUser }}>
         <UserRegisterView />
       </ApiUserRegisterContext.Provider>{" "}
