14cd1c3a889436c599b16b14ecf897bb28ff5995
carlosDeucher <carloshenriquedeucher@gmail.com>
2023-05-25 22:45:47 -0300

    [update] Container recebe sp

--- src/views/components/estructure/container/Container.jsx
+++ src/views/components/estructure/container/Container.jsx
@@ -2,20 +2,24 @@
 
 import { ThemeContext } from "@/contexts/ThemeProvider";
 import { useContext } from "react";
+import Box from "../box/Box";
+import useInlineStyle from "@/hooks/useInlineStyles";
 
-export default function Container({ maxWidth, children }) {
+export default function Container({ maxWidth, children, sp }) {
   const { breakpoints } = useContext(ThemeContext);
+  const styleProps = useInlineStyle(sp);
   return (
     <>
-      <div
-        css={{
+      <Box
+        sp={{
           maxWidth: breakpoints[maxWidth],
           margin: "auto",
-          padding: "15px 20px",
+          padding: [0, "15px 20px"],
+          ...styleProps,
         }}
       >
         {children}
-      </div>
+      </Box>
     </>
   );
 }
