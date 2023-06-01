be4122354f538abb0abe67740920a3c32705438a
carlosDeucher <carloshenriquedeucher@gmail.com>
2023-05-31 21:44:19 -0300

    [update] modal responsivo

--- src/views/pageViews/user/windows/ModalEditarCampo.jsx
+++ src/views/pageViews/user/windows/ModalEditarCampo.jsx
@@ -1,7 +1,9 @@
+import Box from "@/views/components/estructure/box/Box";
 import Modal from "@/views/components/estructure/modal/Modal";
 import Stack from "@/views/components/estructure/stack/Stack";
 import Text from "@/views/components/estructure/text/Text";
 import ButtonContained from "@/views/components/ui/buttons/ButtonContained";
+import ButtonText from "@/views/components/ui/buttons/ButtonText";
 import Input from "@/views/components/ui/input/Input";
 import React from "react";
 
@@ -15,14 +17,34 @@ export default function ModalEditarCampo({
 }) {
   return (
     <Modal
-      sp={{ padding: "25px", paddingTop: "30px", width:"550px" }}
+      sp={{
+        padding: "25px",
+        paddingTop: "30px",
+        width: "500px",
+      }}
       isOpen={isOpen}
       onClose={onClose}
     >
       <Text fontSize="large">Alterar campo</Text>
-      <Input labelText={label} sp={{marginTop:"30px"}}  />
-      <Stack justifyContent="end" columnGap={"10px"} sp={{marginTop:"20px"}}>
-        <ButtonContained onClick={() => onClose}>Salvar</ButtonContained>
+      <Input
+        labelText={label}
+        sp={{
+          marginTop: "30px",
+          width: "100%",
+          minWidth: "100px",
+        }}
+      />
+      <Stack
+        justifyContent="end"
+        rowGap="15px"
+        columnGap={"30px"}
+        sp={{ marginTop: "20px", flexDirection: ["column-reverse", "row"] }}
+      >
+        <ButtonText color="cancel" onClick={onClose}>
+          Cancelar
+        </ButtonText>
+
+        <ButtonContained onClick={onClose}>Salvar</ButtonContained>
       </Stack>
     </Modal>
   );
