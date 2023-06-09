fa6921d3e61da6e5b11a01cfb28ef14b879efba5
carlosDeucher <carloshenriquedeucher@gmail.com>
2023-06-09 18:10:22 -0300

    [add] obj para controlar layout das paginas

---
+++ src/utils/pagesLayout.js
@@ -0,0 +1,20 @@
+export const pagesLayout = {
+  "/home": {
+    renderSidebar: true,
+  },
+  "/create_product": {
+    renderSidebar: true,
+  },
+  "/login": {
+    renderSidebar: false,
+  },
+  "/user/[idUser]": {
+    renderSidebar: true,
+  },
+  "/user_register": {
+    renderSidebar: false,
+  },
+  "/product/[idProduct]": {
+    renderSidebar: true,
+  },
+};
