2d32c47cfe67e064bc8014808339c747ea50dff9
carlosDeucher <carloshenriquedeucher@gmail.com>
2023-06-11 14:19:30 -0300

    [add] sidebar mobile fecha ao alterar pgn

--- src/views/components/ui/pageWrapper/index.jsx
+++ src/views/components/ui/pageWrapper/index.jsx
@@ -1,16 +1,22 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import Header from "./sections/header/Header";
 import Stack from "../../estructure/stack/Stack";
 import SideBar from "../sideBar";
 import Box from "../../estructure/box/Box";
 import { useRouter } from "next/router";
 import { pagesLayout } from "@/utils/pagesLayout";
+import useBreakpoint from "@/hooks/useBreakpoint";
 
 export const SideBarContext = createContext();
 export default function PageWrapper({ children }) {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const router = useRouter();
-  console.log(router.pathname)
+  const isMobile = useBreakpoint("mobile");
+
+  useEffect(() => {
+    if (isMobile) setIsSidebarOpen(false);
+  }, [router]);
+
   return (
     <SideBarContext.Provider value={{ isSidebarOpen, setIsSidebarOpen }}>
       <Stack direction="column" sp={{ height: "100vh", overflow: "hidden" }}>
